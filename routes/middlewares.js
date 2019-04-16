e8cfa24c57297c715b1ccc277514606f573a78fa
DESKTOP-JIU76K2\chj <guswo5580@gmail.com>
2019-04-16 17:20:45 +0900

    Using ApiLimiter is success but Using router.use is fail

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -1,5 +1,8 @@
 const jwt = require('jsonwebtoken');
 const RateLimit = require('express-rate-limit');
+const { Domain } = require('../models');
+
+const cors = require('cors');
 
 exports.isLoggedIn = (req, res, next) => {
   if (req.isAuthenticated()) {
@@ -35,6 +38,22 @@ exports.verifyToken = (req, res, next) => {
   }
 };
 
+exports.Cors = (req, res, next) => {
+  //DB에 저장되어있는 api를 요청한 Domain의 host 정보 get 
+  // console.log(req);
+  const domain = Domain.findOne({
+      where : { host : req.body.host },
+      //요청이 들어온 host에 대해 db의 host와 같은지 비교
+  });
+  if(domain){
+      //db에 일치하는 host가 있는 경우 cors 실행
+      cors({ origin : req.body.host })(req, res, next); 
+  }else {
+      //없다면 이용 x
+      next();
+  }
+};
+
 //api 서버에 대한 사용량 제한
 exports.apiLimiter = new RateLimit({
   windowMs : 60 * 1000,
@@ -64,10 +83,11 @@ exports.premiumApiLimiter = new RateLimit({
     //어겼을 경우 메세지
     res.status(this.satusCode).json({
       code : this.statusCode, //429
-      message : '1분에 한번만 요청해주세요'
+      message : '1분에 60번 만 요청해주세요'
     })
   }
 });
+
 
 //새로운 api 서버를 만들었을 때,
 //기본의 api 서버는 사용이 불가능하도록 
--- routes/v2.js
+++ routes/v2.js
@@ -1,95 +1,99 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 
-const { verifyToken , premiumApiLimiter, apiLimiter} = require('./middlewares');
+const { verifyToken , apiLimiter, premiumApiLimiter ,  Cors } = require('./middlewares');
 const { Domain, User, Post, Hashtag } = require('../models');
 const cors = require('cors');
-const url = require('url');
+const url = require('url-parse');
 
 const router = express.Router();
 
-// router.use(cors());
+router.use(cors());
 //cors 모듈 -- origin header가 없는 origin 변화에 대한 정보 요청 시 발생하는 문제를 해결하는 모듈
 //모든 router에 적용할 것이라면 router.use로 사용 가능 
 
-router.use(async (req, res, next) => {
-    //DB에 저장되어있는 api를 요청한 Domain의 host 정보 get 
-    const domain = await Domain.findOne({
-        where : { host : url.parse(req.get('origin')).host },
-        //요청이 들어온 host에 대해 db의 host와 같은지 비교
-    });
-    if(domain){
-        //db에 일치하는 host가 있는 경우 cors 실행
-        cors({ origin : req.get('origin') })(req, res, next);    
-    }else {
-        //없다면 이용 x
-        next();
-    }
-});
+// router.use( async (req, res, next) => {
+//     console.log('limiter1');
+//     console.log(req.headers);
+//     // const host = req.body.host;
+//     try{
+//         const domain = await Domain.findOne({
+//             where : { host : req.body.host },
+//             //요청이 들어온 host에 대해 db의 host와 같은지 비교
+//         });
 
-router.use( async (req, res, next) => {
-    const domain = await Domain.findOne({
-        where : { host : url.parse(req.get('origin')).host },
-        //요청이 들어온 host에 대해 db의 host와 같은지 비교
+//         if(domain.dataValues.type === "premium"){
+//             premiumApiLimiter(req, res, next);
+//             next();
+//             //domain type에 따라 다른 apiLimiter 를 적용
+//             console.log('success_premium');
+//         }else {//type === free 
+//             apiLimiter(req, res, next);
+//             next();
+//             console.log('success_normal');
+//         }
+//     }catch(error) {
+//         console.error(error);
+//         console.log('Fucking Here');
+//         return res.status(500).json({
+//             code : 500,
+//             message : 'Fail to connect database',
+//             error
+//         });
+//     }
+// });
+
+router.post('/token',  async (req, res) => {
+    const { clientSecret } = req.body;
+    try {
+        const domain = await Domain.findOne({
+        where: { clientSecret },
+        include: {
+            model: User,
+            attribute: ['nick', 'id'],
+        },
     });
-    if(domain.type === 'premium'){
-        premiumApiLimiter(req, res, next);
-        //domain type에 따라 다른 apiLimiter 를 적용
+        if (!domain) {
+            return res.status(401).json({
+                code: 401,
+                message: '등록되지 않은 도메인입니다. 먼저 도메인을 등록하세요',
+            });
+        }
         
-    }else {//type === free 
-        apiLimiter(req, res, next);
+        const token = jwt.sign({
+                id: domain.user.id,
+                nick: domain.user.nick,
+            }, process.env.JWT_SECRET, {
+                expiresIn: '1m', // 1분
+                issuer: 'peaceocean',
+            });
+        return res.json({
+            code: 200,
+            message: '토큰이 발급되었습니다',
+            token,
+        });
+    } catch (error) {
+            console.error(error);
+            return res.status(500).json({
+            code: 500,
+            message: '서버 에러',
+        });
     }
-});
-
-router.post('/token', async (req, res) => {
-  const { clientSecret } = req.body;
-  try {
-    const domain = await Domain.findOne({
-      where: { clientSecret },
-      include: {
-        model: User,
-        attribute: ['nick', 'id'],
-      },
-    });
-    if (!domain) {
-      return res.status(401).json({
-        code: 401,
-        message: '등록되지 않은 도메인입니다. 먼저 도메인을 등록하세요',
-      });
-    }
-    const token = jwt.sign({
-      id: domain.user.id,
-      nick: domain.user.nick,
-    }, process.env.JWT_SECRET, {
-      expiresIn: '1m', // 1분
-      issuer: 'peaceocean',
-    });
-    return res.json({
-      code: 200,
-      message: '토큰이 발급되었습니다',
-      token,
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({
-      code: 500,
-      message: '서버 에러',
-    });
-  }
 });
 
 router.get('/test', verifyToken, (req, res) => {
     res.json(req.decoded);
   });
 
-router.get('/post/mine', verifyToken, (req, res) => {
+router.get('/post/mine', apiLimiter ,verifyToken, async (req, res) => {
     //내가 작성한 게시글을 가져오기
     Post.findAll({
         where : {userId : req.decoded.id}
         //Post db에서 나의 id를 대조하여 모든 post를 가져온다 
     })
+    
     .then((posts) => {
-        console.log(posts);
+        // console.log(posts);
         res.json({
             code : 200,
             payload : posts
@@ -108,9 +112,9 @@ router.get('/post/mine', verifyToken, (req, res) => {
 router.get('/posts/hashtag/:title', verifyToken, async (req, res) => {
     //검색한 해시태그에 대한 게시글을 가져오는 경우 
     try {
-        const hashtag = await Hashtag.find({
+        const hashtag = await Hashtag.findOne({
             //해시태그 db에서 모든 글을 찾는다
-            where : { title : req.params.title}
+            where : { title : req.params.title }
         })
         if(!hashtag){
             //없다면 없음을 반환
@@ -136,7 +140,7 @@ router.get('/posts/hashtag/:title', verifyToken, async (req, res) => {
 router.get('/follower', verifyToken, async (req, res) => {
     //팔로워 목록을 가져오는 경우 
     try {
-        const user = await User.find({
+        const user = await User.findOne({
             where : {id : req.decoded.id}
             //id는 verifyToken에서 설정 decoded 내부의 id와 비교
         });
@@ -160,7 +164,7 @@ router.get('/follower', verifyToken, async (req, res) => {
 
 router.get('/following', verifyToken, async (req, res) => {
     try {
-        const user = await User.find({
+        const user = await User.findOne({
             where : {id : req.decoded.id}
         });
         const following = await user.getFollowings({
